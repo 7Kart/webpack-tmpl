f366105f21de9350402aa34005b74e28509f09f8
7Kart <artgor92@gmail.com>
2019-06-29 23:59:38 +0300

    first commit

---
+++ src/store/example.js
@@ -0,0 +1,12 @@
+export default{
+    state:{
+        message: "Hellow world"
+    },
+    getters:{
+        getMessage(state){
+            return state.message;
+        }
+    },
+    mutations:{},
+    actions:{}
+}
